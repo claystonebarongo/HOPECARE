2e0fa2498979def2c6e59c32e94a3f8ad358736a
Clay <claystonebarongo@gmail.com>
2026-07-13 21:12:27 +0300

    update

--- src/components/DonationPanel.jsx
+++ src/components/DonationPanel.jsx
@@ -127,8 +127,10 @@ export default function DonationPanel() {
                                     <span className="row-label">BANK SWIFT CODE:</span>
                                     <span className="row-val highlight-green">KCOOKENA</span>
                                 </div>
-                                
+
                                 <div className="routing-row">
+
+
                                     <span className="row-label">BRANCH NAME:</span>
                                     <span className="row-val">KISII</span>
                                 </div>
