eedecbf8093bd455f61da3916ead9004343b9a6b
Clay <claystonebarongo@gmail.com>
2026-07-14 23:03:23 +0300

    update

--- src/components/CrusadeSection.jsx
+++ src/components/CrusadeSection.jsx
@@ -52,6 +52,8 @@ export default function CrusadeSection() {
 
                         "Join us in prayer, join us in the field, and let’s labor together for the greatest harvest of souls in Kisii County."
                     </div>
+
+
                 </div>
 
             </div>
