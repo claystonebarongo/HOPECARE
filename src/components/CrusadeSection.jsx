5028ce7b925d5dbf6939db8687c4c62b5f54923d
Clay <claystonebarongo@gmail.com>
2026-07-15 22:50:24 +0300

    update

--- src/components/CrusadeSection.jsx
+++ src/components/CrusadeSection.jsx
@@ -41,6 +41,8 @@ export default function CrusadeSection() {
 
                         Through dynamic open-air crusades, souls are being saved, lives transformed, the sick healed, and many are experiencing the power of God like never before. Pastor Hezborn Omongo Nyandwaro is preaching with boldness and anointing, declaring the good news of salvation across markets, towns, and rural villages, leaving no soul unreached!
                     </p>
+
+
                     <p className="crusade-paragraph">
 
 
