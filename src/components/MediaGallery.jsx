6dde3314f2e0564dc56f9112c763720277fe912c
Clay <claystonebarongo@gmail.com>
2026-07-26 22:49:18 +0300

    update

--- src/components/MediaGallery.jsx
+++ src/components/MediaGallery.jsx
@@ -184,6 +184,7 @@ export default function MediaGallery() {
                             </div>
                         </div>
                     ))}
+
                 </div>
             </div>
 
