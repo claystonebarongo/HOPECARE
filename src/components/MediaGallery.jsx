bc7b005b737d0dc957b79acf76908c47c2b0d9ea
Clay <claystonebarongo@gmail.com>
2026-08-04 22:13:43 +0300

    update

--- src/components/MediaGallery.jsx
+++ src/components/MediaGallery.jsx
@@ -142,12 +142,14 @@ export default function MediaGallery() {
 
                             <span className="showcase-counter">
 
-                            
+
                                 {activePhotoIdx + 1} / {galleryPhotos.length}
 
 
                             </span>
+
                             <h4 className="showcase-caption">{galleryPhotos[activePhotoIdx].title}</h4>
+
                         </div>
                     </div>
                     <button className="showcase-nav-btn next" onClick={handleNextPhoto} aria-label="Next image">❯</button>
