877bb266734c219b22521e27871747568691a96e
Clay <claystonebarongo@gmail.com>
2026-08-06 22:54:35 +0300

    update

--- src/components/MediaGallery.jsx
+++ src/components/MediaGallery.jsx
@@ -58,6 +58,8 @@ export default function MediaGallery() {
             title: 'Daily Welfare & Resource Management',
             desc: 'Providing structure, consistent nutritional frameworks, and protective care pipelines.'
         },
+
+
         {
             id: 'v3',
             videoUrl: 'https://drive.google.com/file/d/1MGuthr8h0OP5OsFxsT5dNuLWoOus1u9B/preview',
